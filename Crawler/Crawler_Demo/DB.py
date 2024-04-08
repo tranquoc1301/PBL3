6d7667f5dbc3ed042cbbe9ab99bed35d25bae339
tranquoc1301 <tranquoc1301@gmail.com>
2024-04-08 21:33:16 +0700

    test

--- Crawler/Crawler_Demo/DB.py
+++ Crawler/Crawler_Demo/DB.py
@@ -55,7 +55,7 @@ def save_job_into_DB(data):
         connection = mysql.connector.connect(user='root', password=DB_Password, host='localhost',
                                              database=DB_Name)
         cursor = connection.cursor()
-        query = "INSERT INTO JOB(COMPANY_ID, JOB_NAME, INDUSTRY, COMPANY_LOCATIONS, POSTED_DATE, ENROLLMENT_LOCATION, `ROLE`, SALARY, GENDER_REQUIREMENT, NUMBER_OF_RECRUITMENT,  AGE_REQUIREMENT, PROBATION_TIME,  WORKWAY, EXPERIENCE_REQUIREMENT, DEGREE_REQUIREMENT, BENEFITS, JOB_DESCRIPTION, JOB_REQUIREMENT, DEADLINE, SOURCE_PICTURE) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"
+        query = "INSERT INTO JOB(COMPANY_ID, JOB_NAME, INDUSTRY, LOCATION, POSTED_DATE, ENROLLMENT_LOCATION, `ROLE`, SALARY, GENDER_REQUIREMENT, NUMBER_OF_RECRUITMENT,  AGE_REQUIREMENT, PROBATION_TIME,  WORKWAY, EXPERIENCE_REQUIREMENT, DEGREE_REQUIREMENT, BENEFITS, JOB_DESCRIPTION, JOB_REQUIREMENT, DEADLINE, SOURCE_PICTURE) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"
         for i in data:
             getCompanyId = "SELECT ID FROM COMPANY WHERE COMPANY_NAME = '" + str(i[0]) + "'"
             companyId = get_company_id_FromDB(getCompanyId)
@@ -87,7 +87,7 @@ def get_data_from_DB():
         connection = mysql.connector.connect(user='root', password=DB_Password, host='localhost',
                                              database=DB_Name)
         cursor = connection.cursor()
-        cursor.execute("SELECT JOB_NAME, INDUSTRY, COMPANY_LOCATIONS, POSTED_DATE, ENROLLMENT_LOCATION FROM JOB ORDER BY JOB_ID")
+        cursor.execute("SELECT JOB_NAME, INDUSTRY, LOCATION, POSTED_DATE, ENROLLMENT_LOCATION FROM JOB ORDER BY JOB_ID")
         data = cursor.fetchall()
         connection.close()
         return data
