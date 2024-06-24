1b4af76a8d6f57e42c7fae9e0b54e3dc66e42c47
tranquoc1301 <tranquoc1301@gmail.com>
2024-06-24 16:51:23 +0700

    fix

--- Backend/scr/helloworldwithexpress.js
+++ Backend/scr/helloworldwithexpress.js
@@ -4,12 +4,10 @@ require('dotenv').config()
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-const port = process.env.port
+const port = process.env.PORT
 const connect = require('./config/database')
 
 const configViewEngine = require('./config/viewEngine');
-
-
 
 configViewEngine(app);
 app.use('/', router)
@@ -17,5 +15,4 @@ app.use('/', router)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-
 })
